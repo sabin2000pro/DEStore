f2df38ee8e9d915f9bfb97862bb3f0e0dd986e4e
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-19 11:19:54 +0100

    Added node modules file

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,20 @@ const dotenv = require('dotenv');
 dotenv.config({path: 'config.env'});
 const port = process.env.PORT;
 const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+const server = app.listen(port, (error) => {
+    if(!error) {
+        return console.log(`Listening for requests on port ${port}`);
+    }
+
+    else {
+        return console.log(`Could not listen for requests on port ${port}`);
+    }
+});
+
+process.on('uncaughtException', (error, promise) => {
+    
+});
