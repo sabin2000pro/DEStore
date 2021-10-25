7df2a8b79de8949b50f59aee36aa658b3620082f
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-25 09:01:13 +0100

    Adding database connection code

---
+++ backend/models/adminModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+    username: {
+
+    },
+
+    email: {
+
+    },
+
+    password: {
+
+    },
+
+    passwordResetToken: String,
+    passwordResetExpires: Date
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+module.exports = Admin;
