cfa4687e7417fb2c6e5c9a1a228e4e97a9dd52c0
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-25 10:56:20 +0100

    Adding Product Routes code

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,8 +2,7 @@ const express = require('express');
 const productRoutes = express.Router();
 const productController = require('../controllers/productsController');
 
-productRoutes.route('/').get(productController.getAllProducts)
-productRoutes.route('/:id')
-
+productRoutes.route('/').get(productController.getAllProducts).post(productController.createProduct)
+productRoutes.route('/:id').get(productController.getProduct).put(productController.editProduct).delete(productController.deleteProduct);
 
 module.exports = productRoutes;
