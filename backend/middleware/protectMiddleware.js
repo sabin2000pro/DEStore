04e51504aad451407843d0d6d61fc432688dba06
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2021-10-25 21:50:23 +0100

    Adding middleware function to protect the product routes

---
+++ backend/middleware/protectMiddleware.js
@@ -0,0 +1,17 @@
+const Admin = require('../models/adminModel');
+const jwt = require('jsonwebtoken');
+
+module.exports.protectProducts = async (request, response, next) => {
+    let token;
+    const headers = request.headers.authorization; // Get the auth headers;
+
+    if(headers && headers.startsWith("Bearer")) {
+        token = headers.split(",")[1]; // Split the token header -> Bearer AAFJAFJ
+    }
+
+    if(!token) {
+
+    }
+
+    
+};
