cf1f20453ccc162b1090f4c149550faf59527e08
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-25 10:40:11 +0100

    Adding code to connect to the database and creating the product model

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const productRoutes = express.Router();
+const productController = require('../controllers/productsController');
 
+
+module.exports = productRoutes;
