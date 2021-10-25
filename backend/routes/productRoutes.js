16af1733dd109e5836e5eb098cb37945d92a58ca
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-25 16:17:07 +0100

    Adding rate limiting to creating products and registering a new admin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const productRoutes = express.Router();
 const productController = require('../controllers/productsController');
+
 const rateLimit = require('express-rate-limit');
+const windowMs = 1 * 60 * 1000;
+const maxRequests = 4;
+
+const limiter = rateLimit({ // Options for rate limiting
+    windowMs,
+    maxRequests,
+    message: `Too many requests coming from the IP: ${ip.address()}. Try again after ${windowMs} MS. You can only make a maximum of ${maxRequests} to prevent DOS attacks`
+ });
 
 
 productRoutes.route('/').get(productController.getAllProducts).post(productController.verifyBody, productController.validateQuantity, productController.createProduct)
