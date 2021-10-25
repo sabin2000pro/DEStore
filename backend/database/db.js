cc804b1742f7ace0abb4551eb11ccb4179e7e364
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-25 10:39:11 +0100

    Adding database connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
+const DB_URI = process.env.DB_URI;
 
-const connectDB = () => {
+const connectDB = async () => { 
+    try {
+        return await mongoose.connect()
+    } 
     
+    catch(error) {
+        if(error) {
+            return console.log(error.toString());
+        }
+    }
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,1 +1,3 @@
 const express = require('express');
+const productRoutes = express.Router();
+
