9afa5e62f45501f435e68071988c4ef08ae67305
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2021-10-25 22:10:00 +0100

    Added comments to the protect middleware function:

--- backend/middleware/protectMiddleware.js
+++ backend/middleware/protectMiddleware.js
@@ -5,23 +5,22 @@ const unauthorized = 401;
 module.exports.protectProducts = async (request, response, next) => { // Middleware function to only grant registered admins the right to create, update and delete products
 
     try {
-        let token;
+        let token; // Get the token
         const headers = request.headers.authorization; // Get the auth headers;
     
-        if(headers && headers.startsWith("Bearer")) {
-            token = headers.split(",")[1]; // Split the token header -> Bearer AAFJAFJ
+        if(headers && headers.startsWith("Bearer")) { // If the header starts with Bearer
+            token = headers.split(",")[1]; // Split the token header to include the full token -> Bearer AAFJAFJ
         }
     
-        if(!token) {
+        if(!token) { // If no token is found
             return response.status(unauthorized).json("You are not authorized to access this route");
         }
     }
     
     catch(error) {
+
         if(error) {
-            
+
         }
     }
-
-
 };
