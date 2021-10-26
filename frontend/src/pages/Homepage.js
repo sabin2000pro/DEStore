f2cc0932fca88210b91ac01a23084954f5041b70
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-26 14:39:08 +0100

    Adding code documentation

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import '../App.css';
+import {useEffect, useState} from 'react';
 
 // Code Author: Sabin Constantin Lungu
 // Code Written on: 25/10/2021
@@ -6,9 +7,39 @@ import React from 'react'
 // Bugs? N/A
 
 const Homepage = () => {
+    const [pageNumber, setPageNumber] = useState(0); // The Current Page Number
+    const [numberOfPages, setNumberOfPages] = useState(0); // Number of Pages Variable
+    const [products, setProducts] = useState([]); // Product State
+
+    const pages = new Array(numberOfPages).fill(null).map((v, i) => i); // Create an array of pages
+
+    useEffect(() => {
+
+        return fetch(`http://localhost:5950/api/v1/products?page=${pageNumber}`).then((response) => response.json()).then(({total, products}) => {
+            console.log(total, products);
+            setProducts(products);
+            setNumberOfPages(total);
+        });
+
+    }, [pageNumber]);
+
     return (
-        <div>
-            <h1>DE-Store Homepage</h1>
+        <div className = "App">
+            <h3>Page {pageNumber + 1}</h3>
+
+            {products.map((product, key) => (
+
+                <div className = "products" key = {key}>
+                    <h4>{product.name}</h4>
+                    <p>{product.description}</p>
+                   
+                </div>
+            ))}
+
+            {pages.map((pageIndex) => (
+                <button onClick = {() => setPageNumber(pageIndex)} >{pageIndex + 1}</button>
+            ))}
+
         </div>
     )
 }
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 
 const RegisterPage = () => {
+    
     return (
         <div>
             
