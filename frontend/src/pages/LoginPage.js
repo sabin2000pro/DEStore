03552100984be460cc2869d917ba57462d47415a
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-30 18:25:21 +0100

    Adding code to register an admin

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginHandler = async (e) => {
+
+    }
+
     return (
         <div>
-            
+            <h1>Admin Login </h1>
+            <form onSubmit = {loginHandler}>
+
+                <div>
+                    <label for = "email">E-mail Address</label>
+                </div>
+
+            </form>
         </div>
     )
 }
