83993fc7420d89fc7402da65459ab1f4c1ddd114
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-31 17:25:08 +0000

    Adding CRUD operations for the payments

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -3,6 +3,7 @@ const paymentRoutes = express.Router();
 const paymentController = require('../controllers/paymentController');
 
 paymentRoutes.route('/getAllPayments').get(paymentController.getAllPayments);
+paymentRoutes.route('/getSinglePayment/:id').get(paymentController.getSinglePayment);
 paymentRoutes.route('/editPayment/:id').put(paymentController.editPayment);
 paymentRoutes.route('/createPayment').post(paymentController.createPayment);
 paymentRoutes.route('/deletePayment/:id').delete(paymentController.deletePayment);
