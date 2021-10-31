f1af42ee0ad263fee4a366214bbf127f7b5e7645
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-31 10:56:49 +0000

    Adding code to the payment routes

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const Payment = require('../models/paymentModel');
 
+module.exports.getAllPayments = async (request, response, next) => {
+
+};
+
 module.exports.createPayment = async (request, response, next) => {
 
 };
 
+module.exports.deletePayment = async (request, response, next) => {
+
+};
--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const paymentRoutes = express.Router();
 const paymentController = require('../controllers/paymentController');
 
+paymentRoutes.route('/getAllPayments').get(paymentController.getAllPayments);
 paymentRoutes.route('/createPayment').post(paymentController.createPayment);
 
 module.exports = paymentRoutes;
