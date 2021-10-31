0eb269b6b5f34c4fda033d233450efddbd904d31
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-10-31 10:58:15 +0000

    Added front-end page for the payment

---
+++ frontend/src/pages/PaymentPage.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import axios from 'axios';
+
+const PaymentPage = () => {
+    return (
+        <div>
+            <h1>Product Payment Page </h1>
+        </div>
+    )
+};
+
+export default PaymentPage;
