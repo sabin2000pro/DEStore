97212c413fd314caf152adfc485365369878d054
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-11-04 21:20:50 +0000

    Added data persistence to docker file

--- frontend/src/presentation/LoginPage.test.js
+++ frontend/src/presentation/LoginPage.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 import ReactDom from 'react-dom';
-import LoginPage from '../pages/RegisterPage';
+import LoginPage from './RegisterPage';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 
