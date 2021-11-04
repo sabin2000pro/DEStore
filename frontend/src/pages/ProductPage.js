589c528283ae0e726ddfcc4203952919037769ab
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-11-04 12:20:40 +0000

    Adding page to view a single product

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -2,13 +2,18 @@ import React, {useEffect} from 'react'
 import {useHistory, useLocation} from 'react-router-dom';
 import axios from 'axios';
 
-const ProductPage = () => {
+const ProductPage = (props) => {
     let location = useLocation();
     let history = useHistory();
+    const {name, image} = location.state.product;
 
     return (
         <div>
-            
+
+            <div>
+                <h1>{name}</h1>
+                <img height = {200} width = {200} src = {image} />
+            </div>
         </div>
     )
 }
