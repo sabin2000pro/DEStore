e6e1e8a98e187ce41ec0cca4b85487524fda3c5c
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-11-04 14:04:48 +0000

    Adding page to view a single product

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -11,6 +11,11 @@ module.exports.getLoyaltyCard = async (request, response, next) => {
     try {
         const payments = await Payment.find();
         const admins = await Admin.find({});
+        const {email} = request.body;
+
+        if(!email) {
+            return response.status(404).json({message: "That e-mail is not available"});
+        }
     } 
     
     catch(error) {
