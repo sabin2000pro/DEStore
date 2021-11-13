61e5e328759605db82f43fea857d7c4abee6c5f0
Sabin Constantin Lungu <sabin2000@SabinLungusMBP.broadband>
2021-11-13 22:50:23 +0000

    Finsihed rendering the orders

--- frontend/src/presentation/AccountingPage.js
+++ frontend/src/presentation/AccountingPage.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
+import '../App.css';
 
 const AccountingPage = () => {
 
@@ -31,14 +32,34 @@ const AccountingPage = () => {
             }
         };
 
+        // "product_ordered": "PS5",
+        // "saleOffer": "Free Delivery",
+        // "customer_name": "John Doe",
+        // "order_address": "123 Fake Address",
+        // "amountPaid": "£480.00",
+        // "datePurchased": "05/11/2021",
+        // "quantity": 1,
+        // "color": "white",
+        // "description": "A nice PS5"
+
     return (
         <div>
             <h1>Accounting Analysis</h1>
+            <h2>DE-Store Orders</h2>
             <button onClick = {() => setDisplay(true)}>View Placed Orders</button>
 
            {displayed ? orders.map((order, key) => {
-               return <div key = {key}>
+               return <div className = "orders" key = {key}>
+                   <h3>Product: {order.product_ordered}</h3>
                    <h3>{order.customer_name}</h3>
+                   <h3>Sale Offer: {order.saleOffer}</h3>
+                   <h3>Order Address: {order.order_address}</h3>
+                   <h3>Amount Paid: {order.amountPaid}</h3>
+                   <h3>Date Purchased: {order.datePurchased}</h3>
+                   <h3>Quantity Purchased: {order.quantity}</h3>
+                   <h3>Order ID: {order._id}</h3>
+                   <h3>Color: {order.color}</h3>
+                   <h3>Product Description: {order.description}</h3>
                </div>
            }) : null}
 
