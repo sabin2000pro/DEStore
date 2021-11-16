bb61c1c6efbf1341da26c3c59c5c08f95d570420
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-11-16 11:09:14 +0000

    Added send e-mail to edit product

--- frontend/src/presentation/FinancePage.js
+++ frontend/src/presentation/FinancePage.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {useLocation} from 'react-router-dom';
+import React, {useState} from 'react'
+import {useLocation, Link} from 'react-router-dom';
 
 const FinancePage = () => {
     let location = useLocation();
@@ -9,9 +9,14 @@ const FinancePage = () => {
         <div>
             <h1>Buy Now Pay Later</h1>
             <h2>Product Name: {name}</h2>
-            Product Image: <img height = {200} width = {200} src = {image} />
+            <img height = {200} width = {200} src = {image} />
             <h2>Product Price: {price}</h2>
             <h2>Product Description: {description}</h2>
+
+                <div>
+                <Link to = {{pathname: `/`} }>Go Back</Link>
+                    
+                </div>
         </div>
     )
 }
