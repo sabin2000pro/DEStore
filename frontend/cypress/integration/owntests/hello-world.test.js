e4c18f26949746ad392aac03362f756c7fd93168
Sabin Constantin Lungu <sabin2000@Sabin-Lungus-Macbook-Pro.local>
2021-11-22 15:10:37 +0000

    Adding E2E tests

--- frontend/cypress/integration/owntests/hello-world.test.js
+++ frontend/cypress/integration/owntests/hello-world.test.js
@@ -1,15 +1,19 @@
 /// <reference types = "cypress" />
 
-describe('Basic Tests', () => {
+describe('Test Suite 1', () => {
 
     it('We have correct page title', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Homepage');
     });
 
-    it('Correct Register page Title', () => {
+    it('Register Page Tests', () => { // Test that page has existing title
         cy.visit('http://localhost:3000/register');
-        cy.contains('Store Manager Registration');
+        cy.contains('Store Manager Registration').should('exist')
+        
+        cy.get('button');
+        cy.get('div');
+        cy.get('.register__txt');
     })
 
 
